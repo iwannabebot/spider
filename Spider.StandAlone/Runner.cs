2d2151725b1c1c2724907fa7554d64c662c59a33
Nilay Vishwakarma <nilayvishwakarma@live.com>
2021-05-13 21:08:14 +0530

    init commit

---
+++ Spider.StandAlone/Runner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Spider.StandAlone
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+            var url = args[0];
+            
+        }
+    }
+}
